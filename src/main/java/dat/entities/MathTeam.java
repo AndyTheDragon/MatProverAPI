8f40f9095e0296d96ace070f9d3ff414c72e4966
MojoOno <Ffranck0406@gmail.com>
2025-03-24 13:17:36 +0100

    added field attributes to MathTeam

--- src/main/java/dat/entities/MathTeam.java
+++ src/main/java/dat/entities/MathTeam.java
@@ -1,5 +1,33 @@
 package dat.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Entity
 public class MathTeam
 {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "math_team_id", nullable = false)
+    private Integer id;
+    private String description;
+
+    @OneToMany
+    private Set<Assignment> assignments;
+
+    @ManyToOne
+    private UserAccount owner;
+
+    @ManyToMany
+    private Set<Question> questions;
+
+    //TODO: Add constructor(s)
+
 }
