ed7d3f469e829b0e5e45da5362a8366fff36a083
Falcho <m.k.falcham@gmail.com>
2025-03-25 11:48:24 +0100

    Added controllers to map, and made relevant method call on map to find correct controller

--- src/main/java/dat/Main.java
+++ src/main/java/dat/Main.java
@@ -2,12 +2,12 @@ package dat;
 
 import dat.config.ApplicationConfig;
 import dat.config.HibernateConfig;
-import dat.controllers.AssignmentController;
-import dat.controllers.MathTeamController;
-import dat.controllers.QuestionController;
-import dat.controllers.SecurityController;
+import dat.controllers.*;
 import dat.routes.Routes;
 import jakarta.persistence.EntityManagerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class Main
@@ -17,11 +17,12 @@ public class Main
 
     public static void main(String[] args)
     {
-        QuestionController questionController = new QuestionController(emf);
-        AssignmentController assignmentController = new AssignmentController(emf);
-        MathTeamController mathTeamController = new MathTeamController(emf);
-        SecurityController securityController = new SecurityController(emf);
-        Routes routes = new Routes(questionController, assignmentController, mathTeamController, securityController);
+        final Map<String, IController> controllers = new HashMap<>();
+        controllers.put("question", new QuestionController(emf));
+        controllers.put("assignment", new AssignmentController(emf));
+        controllers.put("mathTeam", new MathTeamController(emf));
+        controllers.put("security", new SecurityController(emf));
+        Routes routes = new Routes(controllers);
 
         ApplicationConfig
                 .getInstance()
