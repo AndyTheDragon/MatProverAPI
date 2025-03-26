aaca05cea385923da7b9ca1c2bdb0ee3abbe35d8
MojoOno <Ffranck0406@gmail.com>
2025-03-26 10:13:53 +0100

    - Removed unused imports - Added status code 200 - Cleaned up getById() + added status code 200 - Removed logging from create() + added status code 201 + added error handling but is lacking the helper method to precise our error message. - Removed logging from update method + added status code 200 - Cleaned up delete() + added exception handling.

--- src/main/java/dat/controllers/QuestionController.java
+++ src/main/java/dat/controllers/QuestionController.java
@@ -2,17 +2,12 @@ package dat.controllers;
 
 import dat.dao.CrudDAO;
 import dat.dao.HotelDAO;
-import dat.dto.ErrorMessage;
-import dat.dto.HotelDTO;
 import dat.dto.QuestionDTO;
-import dat.entities.Hotel;
 import dat.entities.Question;
-import dat.entities.Room;
 import dat.exceptions.ApiException;
 import io.javalin.http.BadRequestResponse;
 import io.javalin.http.Context;
 import jakarta.persistence.EntityManagerFactory;
-import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -36,7 +31,7 @@ public class QuestionController implements IController, IQuestionController
     {
         try
         {
-            ctx.json(dao.getAll(Question.class));
+            ctx.status(200).json(dao.getAll(Question.class));
         }
         catch (Exception ex)
         {
@@ -48,16 +43,17 @@ public class QuestionController implements IController, IQuestionController
     @Override
     public void getById(Context ctx)
     {
-
-        try {
-            //long id = Long.parseLong(ctx.pathParam("id"));
+        try
+        {
             long id = ctx.pathParamAsClass("id", Long.class)
-                    .check(i -> i>0, "id must be at least 0")
+                    .check(i -> i > 0, "id must be at least 0")
                     .getOrThrow((validator) -> new BadRequestResponse("Invalid id"));
             QuestionDTO foundEntity = new QuestionDTO(dao.getById(Question.class, id));
-            ctx.json(foundEntity);
+            ctx.status(200).json(foundEntity);
 
-        } catch (Exception ex){
+        }
+        catch (Exception ex)
+        {
             logger.error("Error getting entity", ex);
             throw new ApiException(404, "No content found for this request");
         }
@@ -68,20 +64,15 @@ public class QuestionController implements IController, IQuestionController
     {
         try
         {
-            logger.info("Received request to create a new Question");
             QuestionDTO incomingTest = ctx.bodyAsClass(QuestionDTO.class);
-            logger.info("Request data: {}", incomingTest);
-
             Question entity = new Question(incomingTest);
             Question createdEntity = dao.create(entity);
-
-            logger.info("Created new Question with ID: {}", createdEntity.getId());
             ctx.status(201).json(new QuestionDTO(createdEntity));
         }
         catch (Exception ex)
         {
             logger.error("Error creating entity", ex);
-            throw new ApiException(400, "Field ‘xxx’ is required"); //Jeg ved ikke hvordan jeg gør `xxx´ dynamisk.
+            throw new ApiException(400, "Field ‘xxx’ is required"); //TODO Lav hjælpe metode til fejlmelding
         }
     }
 
@@ -89,15 +80,13 @@ public class QuestionController implements IController, IQuestionController
     {
         try
         {
-            logger.info("Received request to update a Question");
             QuestionDTO questionDTO = ctx.bodyAsClass(QuestionDTO.class);
-            logger.info("Request data: {}", questionDTO);
 
-            if (questionDTO.getId() == null || questionDTO.getId() <= 0) {
+            if (questionDTO.getId() == null || questionDTO.getId() <= 0)
+            {
                 logger.warn("Invalid id: {}", questionDTO.getId());
                 throw new BadRequestResponse("Invalid id");
             }
-            logger.info("Updating Question with ID: {}", questionDTO.getId());
 
             Question questionToUpdate = dao.getById(Question.class, questionDTO.getId());
 
@@ -113,8 +102,7 @@ public class QuestionController implements IController, IQuestionController
             updateFieldIfNotNull(questionToUpdate::setTestFormat, questionDTO.getTestFormat());
 
             Question updatedEntity = dao.update(questionToUpdate);
-            logger.info("Successfully updated Question with ID: {}", updatedEntity.getId());
-            ctx.json(new QuestionDTO(updatedEntity));
+            ctx.status(200).json(new QuestionDTO(updatedEntity));
         }
         catch (Exception ex)
         {
@@ -129,7 +117,7 @@ public class QuestionController implements IController, IQuestionController
         {
             //long id = Long.parseLong(ctx.pathParam("id"));
             long id = ctx.pathParamAsClass("id", Long.class)
-                    .check(i -> i>0, "id must be at least 0")
+                    .check(i -> i > 0, "id must be at least 0")
                     .getOrThrow((validator) -> new BadRequestResponse("Invalid id"));
             dao.delete(Question.class, id);
             ctx.status(204);
@@ -137,14 +125,15 @@ public class QuestionController implements IController, IQuestionController
         catch (Exception ex)
         {
             logger.error("Error deleting entity", ex);
-            ErrorMessage error = new ErrorMessage("Error deleting entity");
-            ctx.status(400).json(error);
+            throw new ApiException(404, "No content found for this request");
         }
     }
 
     @Override
-    public <T> void updateFieldIfNotNull(Consumer<T> setter, T value) {
-        if (value != null) {
+    public <T> void updateFieldIfNotNull(Consumer<T> setter, T value)
+    {
+        if (value != null)
+        {
             setter.accept(value);
         }
     }
