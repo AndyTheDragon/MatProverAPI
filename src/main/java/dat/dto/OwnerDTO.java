36635087587d29934c995744a9d60772819c65cd
Falcho <m.k.falcham@gmail.com>
2025-03-27 10:14:41 +0100

    Changed attribute name to match userAccount entity

--- src/main/java/dat/dto/OwnerDTO.java
+++ src/main/java/dat/dto/OwnerDTO.java
@@ -12,14 +12,14 @@ public class OwnerDTO
 {
     private Integer id;
     private String name;
-    private String school;
+    private String workplace;
     private String email;
 
     public OwnerDTO(UserAccount user)
     {
         this.id = user.getId();
         this.name = user.getName();
-        this.school = user.getWorkplace();
+        this.workplace = user.getWorkplace();
         this.email = user.getEmail();
     }
 }
