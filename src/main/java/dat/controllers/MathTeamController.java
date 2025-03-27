7ab7b9be033ceced3ee05bbe205011a1d4143601
idrissalvatoreisci <158306705+idrissalvatoreisci@users.noreply.github.com>
2025-03-27 14:34:33 +0100

    minor

--- src/main/java/dat/controllers/MathTeamController.java
+++ src/main/java/dat/controllers/MathTeamController.java
@@ -1,16 +1,22 @@
 package dat.controllers;
 
+import dat.config.HibernateConfig;
 import dat.dao.CrudDAO;
 import dat.dao.GenericDAO;
 import dat.dto.ErrorMessage;
+import dat.dto.MathTeamDTO;
 import io.javalin.http.Context;
+import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 public class MathTeamController implements IController
 {
     private final CrudDAO dao;
-
+    private final Logger logger = LoggerFactory.getLogger(MathTeamController.class);
+    private  final EntityManagerFactory emf = HibernateConfig.getEntityManagerFactoryForTest();
     public MathTeamController(EntityManagerFactory emf)
     {
         dao = new GenericDAO(emf);
@@ -19,6 +25,15 @@ public class MathTeamController implements IController
     @Override
     public void create(Context ctx)
     {
+        try
+        {
+            ctx.json(MathTeamDTO.class);
+        } catch (Exception e)
+        {
+            logger.error("unable to create object", e);
+            ErrorMessage err = new ErrorMessage("Error persisting to db");
+            ctx.json(err);
+        }
         ErrorMessage error = new ErrorMessage("Error, not implementet yet");
         ctx.status(501).json(error);
     }
