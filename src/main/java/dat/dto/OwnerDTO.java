ccba98a68389206d93888f3bc20f72ce87098cb6
Falcho <m.k.falcham@gmail.com>
2025-03-27 09:56:01 +0100

    Added lombok

--- src/main/java/dat/dto/OwnerDTO.java
+++ src/main/java/dat/dto/OwnerDTO.java
@@ -1,7 +1,13 @@
 package dat.dto;
 
 import dat.entities.UserAccount;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class OwnerDTO
 {
     private Integer id;
