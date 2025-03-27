b2fec4710369903ddac9271bd9f0a720f7857273
idrissalvatoreisci <158306705+idrissalvatoreisci@users.noreply.github.com>
2025-03-27 11:22:20 +0100

    updated field var  names

--- src/main/java/dat/dto/MathTeamDTO.java
+++ src/main/java/dat/dto/MathTeamDTO.java
@@ -1,21 +1,31 @@
 package dat.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import dat.entities.Assignment;
 import dat.entities.MathTeam;
 import dat.entities.Question;
 import dat.entities.UserAccount;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import java.util.List;
 import java.util.Set;
 
 @Getter
+@NoArgsConstructor
+@JsonIgnoreProperties
 public class MathTeamDTO
 {
     private Integer id;
+    @JsonProperty("desc")
     private String description;
     private Set<Assignment> assignments;
     private UserAccount owner;
     private Set<Question> questions;
+    @JsonProperty("used_examquestions")
+    private List<Question> usedExamQuestions;
+    private String level;
 
     public MathTeamDTO(MathTeam mathTeam)
     {
